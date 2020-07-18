e3a2a27848d47ec6432cb85e634a6a4392f022af
Sophia Pearson <scpearson94@byui.edu>
2020-07-17 18:21:51 -0700

    listen and set up index.ejs

--- db/project2.sql
+++ db/project2.sql
@@ -1,10 +1,3 @@
 #product_type
 INSERT INTO account_type (type) VALUES ('checking');
 INSERT INTO account_type (type) VALUES ('savings');
-
-#product
-INSERT INTO product (product_type_id, name, price, image_url) VALUES ('1', 'Ameraucana', '3.35', 'ameraucana.jpg');
-
-#shipping_type
-INSERT INTO shipping_type (name, price, days) VALUES ('Fedex', '13.95', '7');
-
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
+const debug = require('debug')('project2:server');
 const express = require("express");
+const http = require('http');
+const path = require('path');
 const app = express();
 const { Pool } = require("pg");
 const port = normalizePort(process.env.PORT || '3000');
@@ -8,7 +11,64 @@ const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({connectionString: connectionString});
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.set('port', port);
+var indexRouter = require('./routes/index');
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .set('port', port)
+  .use('/', indexRouter)
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 /**
  * Test query
@@ -20,11 +80,10 @@ pool.query(sql, function(err, result) {
     if (err) {
         console.log("Error in query: ")
         console.log(err);
-    } else {
+    }
       // Log this to the console for debugging purposes.
       console.log("Back from DB with result:");
       console.log(result.rows);
-    }
 
 }); // end of test query
 
